7eab4ace7e0c4d97c994347b78ee6106b43c511a
Swastikyadav <swastikyadav.dev@gmail.com>
2020-04-03 15:10:42 +0530

    made header responsive with meida query

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,33 +1,55 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-function Menu() {
-  const activeStyles = {
-    borderTop: '5px solid #3FBBC9'
+class Menu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      mobile_menu_open: false
+    }
   }
 
+  activeStyles = {
+    borderTop: '5px solid #3FBBC9',
+  }
+
+  handleMenuToggle = () => {
+    this.setState({
+      mobile_menu_open: !this.state.mobile_menu_open
+    });
+  }
+
+  render() {
   return(
-    <ul className="menu-list">
-      <Link to="/" activeStyle={activeStyles}>
-        <li>Home</li>
-      </Link>
-      <Link to="/javascript" activeStyle={activeStyles}>
-        <li>JavaScript</li>
-      </Link>
-      <Link to="/reactjs" activeStyle={activeStyles}>
-        <li>ReactJS</li>
-      </Link>
-      <Link to="/ruby" activeStyle={activeStyles}>
-        <li>Ruby</li>
-      </Link>
-      <Link to="/ruby-on-rails" activeStyle={activeStyles}>
-        <li>Ruby on Rails</li>
-      </Link>
-      <Link to="/learn" activeStyle={activeStyles}>
-        <li>Learn</li>
-      </Link>
-  </ul>
+    <>
+      <ul className="menu-list" style={this.state.mobile_menu_open ? {display: 'block'} : null}>
+        <Link to="/" activeStyle={this.activeStyles}>
+          <li>Home</li>
+        </Link>
+        <Link to="/javascript" activeStyle={this.activeStyles}>
+          <li>JavaScript</li>
+        </Link>
+        <Link to="/reactjs" activeStyle={this.activeStyles}>
+          <li>ReactJS</li>
+        </Link>
+        <Link to="/ruby" activeStyle={this.activeStyles}>
+          <li>Ruby</li>
+        </Link>
+        <Link to="/ruby-on-rails" activeStyle={this.activeStyles}>
+          <li>Ruby on Rails</li>
+        </Link>
+        <Link to="/learn" activeStyle={this.activeStyles}>
+          <li>Learn</li>
+        </Link>
+    </ul>
+    <h2 className="arrow" onClick={this.handleMenuToggle}>
+      {
+        this.state.mobile_menu_open ? (<>&uarr;</>) : (<>&darr;</>)
+      }
+    </h2>
+  </>
   );
+  }
 }
 
 export default Menu;
