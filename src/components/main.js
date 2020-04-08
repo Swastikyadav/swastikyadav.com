817a97bfdc01c8e1b10b163d8a0a15fec120b0f8
Swastikyadav <swastikyadav.dev@gmail.com>
2020-04-08 16:32:43 +0530

    add about me section and profile pic using gatsby StaticQuery

--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import Img from 'gatsby-image';
+import { StaticQuery, graphql } from 'gatsby';
 
-function Main({ children, aside_display }) {
-  return(
-    <main style={aside_display ? {justifyContent: 'center'} : null}>
-      <div className="blog-list">
-        {children}
-      </div>
-      <aside style={aside_display ? {display: 'none'} : null} className="aside">About Me</aside>
-    </main>
-  );
-}
-
-export default Main;
+export default ({children, aside_display}) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        file(relativePath: { eq: "profile.png" }) {
+          childImageSharp {
+            fixed(width: 125, height: 125) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <main style={aside_display ? {justifyContent: 'center'} : null}>
+        {console.log(data.file.childImageSharp.fixed.src)}
+        <div className="blog-list">
+          {children}
+        </div>
+        <aside style={aside_display ? {display: 'none'} : null} className="aside">
+          <div className="profile-card">
+            <Img fixed={data.file.childImageSharp.fixed} style={{borderRadius: '50%', border: '5px solid #3FBBC9'}} />
+            <p>
+              <strong>Hi! I am Swastik</strong>, a full stack software developer,
+              writting about modern JavaScript, React JS, Ruby, Ruby On Rail and development.
+              Have any query? Send me an email at <strong>swastikb380@gmail.com</strong>
+            </p>        
+          </div>
+        </aside>
+      </main>
+    )}
+  />
+);
--- src/pages/learn.js
+++ src/pages/learn.js
@@ -7,7 +7,7 @@ function Learn() {
   return(
     <Layout>
       <SEO title="Learn in public" />
-      <p>Learn Page!</p>
+      <p>Learn Page! Under Construction.</p>
     </Layout>
   );
 }
