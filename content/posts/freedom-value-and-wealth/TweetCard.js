f14e0bae9844e7911f37a7351133dfe8dfdf4217
Swastikyadav <swastikyadav.dev@gmail.com>
2021-01-04 22:47:54 +0530

    added twitter card

---
+++ content/posts/freedom-value-and-wealth/TweetCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Tweet } from "react-twitter-widgets";
+
+function TweetCard() {
+  return (
+    <Tweet tweetId="1346127172193243137" />
+  );
+}
+
+export default TweetCard;
