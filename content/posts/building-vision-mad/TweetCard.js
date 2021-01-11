3c3fd79e3c4934c90d10ad8040ace1bb8daf4078
Swastikyadav <swastikyadav.dev@gmail.com>
2021-01-12 00:07:11 +0530

    added twitter card to VisionMad post

---
+++ content/posts/building-vision-mad/TweetCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Tweet } from "react-twitter-widgets";
+
+function TweetCard() {
+  return (
+    <Tweet tweetId="1348580317880221701" />
+  );
+}
+
+export default TweetCard;
