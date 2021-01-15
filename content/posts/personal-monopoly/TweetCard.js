d84aedf484c991fedaf05a892643d6f7b651e575
Swastikyadav <swastikyadav.dev@gmail.com>
2021-01-15 22:36:58 +0530

    added personal-monopoly post

---
+++ content/posts/personal-monopoly/TweetCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Tweet } from "react-twitter-widgets";
+
+function TweetCard() {
+  return (
+    <Tweet tweetId="1349230331749048322" />
+  );
+}
+
+export default TweetCard;
