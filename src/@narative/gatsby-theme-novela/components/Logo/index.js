18a67a5b05371ed1a1af31cf25d0ed6a45dc8c9f
Swastikyadav <swastikyadav.dev@gmail.com>
2021-04-20 01:23:38 +0530

    added Subscription Shadow component

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from "gatsby";
 
 import "../../../../assets/stylesheets/custom.css";
 
 export default function Logo() {
   return (
-      <div><h1 className="logo">Swastik <span>Yadav</span></h1></div>
+      <div className="logo-container">
+        <h1 className="logo full-name">Swastik <span>Yadav</span></h1>
+        <h1 className="logo short-name">S <span>Y</span></h1>
+        {/* <Link to="/courses">Courses</Link> */}
+      </div>
   );
 }
---
+++ src/@narative/gatsby-theme-novela/components/Subscription/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import SubscriptionForm from "../../../../components/Subscription";
+
+function Subscription() {
+  return (
+    <SubscriptionForm />
+  );
+}
+
+export default Subscription;
