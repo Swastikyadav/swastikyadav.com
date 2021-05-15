5e70dbae1bb99df6de18a8edd6b50198fc69896f
Swastikyadav <swastikyadav.dev@gmail.com>
2021-05-15 17:29:36 +0530

    updated twitter handle

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/codeSwastik`,
+        url: `https://twitter.com/swastikJsDev`,
       },
       {
         name: `github`,
