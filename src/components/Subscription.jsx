c24009b05b1ce41a9acfbe513203abde8e017ddf
Swastik Yadav <swastikb380@gmail.com>
2024-02-04 13:21:17 +0530

    Update Subscription.jsx
    Link subscription to DiaryOfADev newsletter.

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -5,16 +5,16 @@ import "../assets/stylesheets/subscribeForm.css";
 function Subscription() {
   return (
     <div id="revue-embed">
-      <h3>Developers, <span className="highlight">Double your income</span> without competing against <span className="highlight">thousands of resumes</span> by building your <span className="highlight">social presence</span>.</h3>
+      <h3><span className="highlight">DiaryOfADev</span>, Success stories of underdog developers.</h3>
       <p>
-        Learn to bypass all the competition. Let the opportunities come to you instead of you mindlessly running behind it. This is how you monetize your webDev skills.
+        Every week we share an underdog developer's story and a deep dive into a programming concept.
       </p>
 
       {/* <form action="https://www.getrevue.co/profile/8020lessons/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank"> */}
       {/* <input className="revue-form-field" placeholder="Your @email.com" type="email" name="member[email]" id="member_email" required /> */}
 
-      <a href="https://diaryofadev.ck.page/a4a719c8f3" target="_blank" rel="noopener noreferrer">
-        <button className="revue-form-submit" id="member_submit">2x Income Now!</button>
+      <a href="https://diaryofadev.in/" target="_blank" rel="noopener noreferrer">
+        <button className="revue-form-submit" id="member_submit">Join The Newsletter</button>
       </a>
 
       {/* <small className="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</small> */}
