38eaadb551a473e37008d6800b75f7f47229e88a
Swastik Yadav <swastikb380@gmail.com>
2024-07-15 18:47:56 +0530

    Update config site and add blogs

--- components/mode-toggle.jsx
+++ components/mode-toggle.jsx
@@ -11,11 +11,11 @@ export function ModeToggle() {
     <Button variant="ghost" className="w-10 px-0">
       <Sun
         onClick={() => setTheme("dark")}
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
       />
       <Moon
         onClick={() => setTheme("light")}
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        className="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
       />
     </Button>
   );
--- config/site.js
+++ config/site.js
@@ -1,24 +1,35 @@
 import { Icons } from "@/components/icons";
 
 export const siteConfig = {
-  name: "NextJs blog boilerplate",
-  url: "",
-  description: "A minimal boilerplate to move fast with blogging in NextJs",
-  author: "Blog Fast",
-  authorProfileImg: "/images/blogfast.png",
-  about: "Write few words about your self.",
+  name: "Swastik's blog built with BlogFast",
+  url: "https://swastikyadav.com/",
+  description: "My portfolio of small bets.",
+  author: "Swastik Yadav",
+  authorProfileImg:
+    "https://media.dev.to/cdn-cgi/image/width=320,height=320,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F624038%2F06e37841-8bee-420a-a76e-4f05684aaf1c.jpg",
+  about:
+    "Software Engineer (3+ Years Exp) || JavaScript, React, NextJs || Writes about JavaScript & life. Building side projects used by real people. || My blogs on Dev.To have been read by 200k+ readers so far.",
   links: [
-    { name: "github", url: "" },
-    { name: "linkedin", url: "" },
+    { name: "github", url: "https://github.com/swastikyadav" },
+    { name: "linkedin", url: "https://linkedin.com/in/swastikyadav" },
   ],
   projects: [
     // Add more project objects here...
     {
-      name: "Project Name",
-      description: "A small description about the project",
+      name: "BlogFast",
+      description:
+        "A NextJs boilerplate for developers to start blogging in hours not weeks.",
       url: "",
       isActive: true,
       logo: "/images/blogfast.png",
+    },
+    {
+      name: "TwEmbed",
+      description:
+        "Embed your twitter profile on any website with just copy & paste script.",
+      url: "https://twembed.netlify.app/",
+      isActive: false,
+      logo: "/images/twembed.png",
     },
   ],
 };
