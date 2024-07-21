1bbc426e81301ee87f58173ec8a383bf4f6d5907
Swastik Yadav <swastikb380@gmail.com>
2024-07-21 21:54:11 +0530

    NewBlog: Add server vs client component blog

--- app/blog/[...slug]/page.jsx
+++ app/blog/[...slug]/page.jsx
@@ -1,35 +1,35 @@
 import { posts } from "#site/content";
 import { MDXContent } from "@/components/mdx-components";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 import "@/styles/mdx.css";
 
 async function getPostFromParams(params) {
-    const slug = params?.slug?.join("/");
-    const post = posts.find(post => post.slugAsParams === slug);
+  const slug = params?.slug?.join("/");
+  const post = posts.find((post) => post.slugAsParams === slug);
 
-    return post;
+  return post;
 }
 
-export default async function PostPage({ params}) {
-    const post = await getPostFromParams(params);
+export default async function PostPage({ params }) {
+  const post = await getPostFromParams(params);
 
-    if (!post || !post.published) {
-        notFound();
-    }
+  if (!post || !post.published) {
+    notFound();
+  }
 
-    return (
-        <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
-            <h1 className="mb-2">{post.title}</h1>
-            {
-                post.description ? (
-                    <p className="text-xl mt-0 text-muted-foreground">
-                        {post.description}
-                    </p>
-                ) : null
-            }
-            <hr className="my-4" />
-            <MDXContent code={post.body} />
-        </article>
-    );
+  return (
+    <article className="container max-w-3xl py-6 mx-auto prose dark:prose-invert">
+      <Link href="/" className="no-underline">
+        <small>&#8592; Back To Home!</small>
+      </Link>
+      <h1 className="my-2">{post.title}</h1>
+      {post.description ? (
+        <p className="mt-0 text-xl text-muted-foreground">{post.description}</p>
+      ) : null}
+      <hr className="my-4" />
+      <MDXContent code={post.body} />
+    </article>
+  );
 }
