febc2c2f82641d896c40ce300748116fe486a96f
Swastik Yadav <swastikb380@gmail.com>
2024-09-02 22:39:06 +0530

    integrate convertkit form

--- app/jsmentalmodels/page.jsx
+++ app/jsmentalmodels/page.jsx
@@ -43,21 +43,26 @@ export default function JsMentalModel({ searchParams: { success } }) {
         </Accordion>
 
         <div className="my-8">
-          <form action="">
+          <form
+            action="https://app.convertkit.com/forms/7041351/subscriptions"
+            method="post"
+          >
             {/* <input type="text" name="first_name" /> */}
             <input
               type="email"
               name="email_address"
+              id="email_address"
               placeholder="3 new Mental Models every Tuesday."
               className="w-full p-2 mb-2 border rounded border-slate-400"
+              required
             />
             <Button type="submit" className="w-full bg-[#597cff]">
-              I'm In
+              {`Yes! I'm In :)`}
             </Button>
             {success && (
               <small className="font-semibold text-green-600">
-                Success! Now check your email for a surprise gift. Also check
-                your spam folder if can't find the email.
+                Success! Now check your email to confirm the subscription. Also
+                check your spam folder if can't find the email.
               </small>
             )}
           </form>
