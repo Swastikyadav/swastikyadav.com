f840779c4c1eca4e19f823fa330b169ba64ca103
Swastik Yadav <swastikb380@gmail.com>
2024-09-03 11:45:59 +0530

    add exitIntent subscribe popup

--- app/jsmentalmodels/page.jsx
+++ app/jsmentalmodels/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { mentalModels } from "@/config/mentalModels";
 import Image from "next/image";
 
@@ -10,10 +11,34 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 import { MentalModelDrawer } from "@/components/mental-model-drawer";
 import { MentalModelSubscriptionForm } from "@/components/mental-model-subscription";
 
 export default function JsMentalModel({ searchParams: { success } }) {
+  const [isSubPopupOpen, setIsSubPopupOpen] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth >= 1024) {
+      document.addEventListener("mouseout", (e) => {
+        if (e.toElement === null && e.relatedTarget === null) {
+          setIsSubPopupOpen(true);
+        }
+      });
+    } else {
+      setTimeout(() => {
+        setIsSubPopupOpen(true);
+      }, 60000);
+    }
+  }, []);
+
   return (
     <div className="block sm:flex">
       <aside className="sm:w-[450px] border-r-2 shadow-xl py-16 px-4 relative">
@@ -53,6 +78,40 @@ export default function JsMentalModel({ searchParams: { success } }) {
           return <MentalModelDrawer model={model} />;
         })}
       </section>
+
+      <Dialog
+        open={isSubPopupOpen}
+        onOpenChange={() => setIsSubPopupOpen(false)}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              <Image
+                src={"/images/mentalModels/jsmmlogo.png"}
+                width={250}
+                height={83}
+                className="m-auto border-b"
+              />
+            </DialogTitle>
+            <DialogDescription className="m-auto">
+              <p>Hey, I'm Swastik. Every Tuesday I share:</p>
+              <br />
+              <p>- 3 Frontend interview tips</p>
+              <p>- 2 JS mental models</p>
+              <p>- 1 Favourite blog from web</p>
+              <br />
+              <p>
+                It is subscribed by beginners in 1st year and devs in MAANG.
+              </p>
+              <br />
+              <p className="underline">1.3k developers enjoy it.</p>
+              <div className="w-full m-auto md:w-96">
+                <MentalModelSubscriptionForm />
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
