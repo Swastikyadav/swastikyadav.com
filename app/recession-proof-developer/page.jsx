ff9cc26f966646594fdd61069871fb3570feee92
Swastik Yadav <swastikb380@gmail.com>
2024-11-10 14:00:56 +0530

    add trpd page

---
+++ app/recession-proof-developer/page.jsx
@@ -0,0 +1,48 @@
+export default function RecessionProofDev() {
+  return (
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <h2 className="text-2xl">
+        Hey, there{" "}
+        <span className="font-semibold text-purple-600">
+          Thanks for your trust
+        </span>{" "}
+        and congrats on choosing to become{" "}
+        <span className="font-semibold text-purple-600">
+          "The Recession Proof Developer".
+        </span>{" "}
+      </h2>
+
+      <div className="grid col-span-1 gap-2 p-2 my-8 border rounded">
+        <h2 className="text-xl">
+          The videos go live on{" "}
+          <span className="font-semibold text-purple-600">
+            "13th Nov 2024 - Wednesday"
+          </span>{" "}
+          . Meanwhile join the email list if you haven't already.
+          <br />
+          <br />
+          If you are in the emails list it will also be easier to send you the
+          videos on the launch day.
+          <br />
+          <br />
+          So, make sure you're in there.
+          <br />
+          <br />
+          Step #1: Visit this{" "}
+          <a
+            href="https://degreelessdev.kit.com/d510f22648"
+            className="text-blue-600 underline"
+          >
+            link
+          </a>
+          . <br />
+          Step #2: Submit your email. <br />
+          Step #3: Check your email to confirm subscription.
+          <br />
+          <br />
+          Thank You!
+        </h2>
+      </div>
+    </div>
+  );
+}
